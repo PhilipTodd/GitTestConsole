d4eef098c73bc40cc81662970b7989819fd6678f
Philip Todd <philip@fishvision.com>
2014-02-10 13:23:43 +1100

    commit 1

---
+++ GitTestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a change
+            Console.ReadLine();
+            Console.Write("foo");
+        }
+    }
+}
